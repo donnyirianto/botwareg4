bc6c2f107d4e3d047c3f5153cf4d9408fd43c612
zaranggi <donnyirianto.anggriawan@gmail.com>
2023-03-30 09:10:21 +0700

    update api get data IP toko

--- helpers/iptoko.js
+++ helpers/iptoko.js
@@ -8,7 +8,10 @@ const bykdcab =  async(kdcab) => {
     }
     const results = await axios.post('http://172.24.52.3:4646/user/GET_Tokomain', payload)
     
-    return results.data
+    const resp = {
+      data : JSON.parse(results.data.data)
+    }
+    return resp
    } catch(err) {  
      return "Gagal"     
   } 
@@ -21,8 +24,10 @@ const bykdtk =  async(kdcab,kdtk) => {
       "IN_KODE_CABANG": `${kdcab}`, "IN_KODE_TOKO":`${kdtk}`,"IN_STATION": "01"
     } 
     const results = await axios.post('http://172.24.52.3:4646/user/GET_Tokomain', payload)
-    //console.log(results.data)
-    return results.data
+    const resp = {
+      data : JSON.parse(results.data.data)
+    }
+    return resp
    } catch(err) { 
      console.log(err)     
      return "Gagal"     
